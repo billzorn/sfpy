b777b3f3e24fd07f27b1abc087da46b5fdc823be
Bill Zorn <bill.zorn@gmail.com>
2018-08-09 13:37:21 -0700

    change setup.py to use setuptools

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
-from distutils.core import setup
-from distutils.extension import Extension
+import setuptools
 from Cython.Build import cythonize
 
-posit_ext = Extension(
+posit_ext = setuptools.Extension(
     'sfpy.posit', ['sfpy/posit.pyx'],
     include_dirs=['SoftPosit/source/include/'],
     extra_objects=['./SoftPosit/build/Linux-x86_64-GCC/softposit.a'],
     libraries=['m'],
 )
 
-float_ext = Extension(
+float_ext = setuptools.Extension(
     'sfpy.float', ['sfpy/float.pyx'],
     include_dirs=['berkeley-softfloat-3/source/include/'],
     extra_objects=['./berkeley-softfloat-3/build/Linux-x86_64-GCC/softfloat.a'],
 )
 
-setup(
+setuptools.setup(
     name='sfpy',
     version='0.1.0',
     description='softfloat and softposit in python',
